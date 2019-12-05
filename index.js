bd15efcc1e5579dca803a0489858019356a9ab35
Eric S. Londres <elondres@stevens.edu>
2019-12-04 19:40:48 -0500

    removed hello world

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const WebSocket = require('ws');
 const server = new WebSocket.Server({ port: 3080, clientTracking: true });
 
 server.on('connection', ws => {
-    ws.send('Hello world');
     ws.on('message', message => {
 	console.log(`Sending ${message} to all clients`);
 	server.clients.forEach(client => client.send(message));
